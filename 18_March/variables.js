e4fd0b565485e73912b969317c903ef79b853f09
yesudassebastian <yesudasseb@gmail.com>
2023-03-18 20:26:22 +0530

    Learnt about Variables

---
+++ 18_March/variables.js
@@ -0,0 +1,11 @@
+let firname = "John "
+let secname = "Doe"
+let fullname = firname+secname
+// console.log(fullname)
+let birthyear = 1996
+const presentyear = 2023
+let age = presentyear-birthyear
+// console.log(age)
+let grade = 5
+
+console.log("My name is "+fullname+". "+"My age is "+age+". "+"I'm studying in "+grade+"th grade"+".")
