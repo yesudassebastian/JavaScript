c35b75024477007620723e3089a43ce814c78925
yesudassebastian <yesudasseb@gmail.com>
2023-03-19 17:31:52 +0530

    Created a Resume of Urmila Nambiar

---
+++ 19_March/Resume.js
@@ -0,0 +1,32 @@
+let firstName = "Urmila";
+let secondName = "Nambiar";
+let fullName = firstName + " " + secondName;
+
+let birthDate = 18;
+let birthMonth = 02;
+let birthYear = 2000;
+let currentYear = 2023;
+
+let Age = currentYear - birthYear;
+let courseEnrolled = "Msc.Psychology";
+
+let district = "Mumbai";
+let state = "Maharashtra";
+
+let hobby1 = "singing";
+let hobby2 = "reading books";
+let hobby3 = "watching TV";
+
+let endingNote = "Thank You!";
+
+console.log(`
+
+    ${fullName}\n
+    ${birthYear}-${birthMonth}-${birthDate}\n
+    My name is ${fullName}.\n
+    I'm ${Age} years old and I'm currently enrolled in ${courseEnrolled}.\n
+    I'm coming from ${district} district of ${state}.\n
+    My hobbies are ${hobby1}, ${hobby2} and ${hobby3}.\n
+    ${endingNote}
+
+    `);
